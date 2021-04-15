b0f829be2e46955ad98f97aaacb6b63676f3c892
diegomartinez13 <diego.martinez13@upr.edu>
2021-04-14 22:07:41 -0400

    Sierpiński triangle added (only one color)

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -69,6 +69,7 @@ void ofApp::drawMode3(float x, float y, float size, int n){
 
     drawMode3(x, y, size / 2, n - 1);
     drawMode3((a.x + b.x) / 2, (a.y + b.y) / 2, size / 2, n - 1);
+    drawMode3((a.x + c.x) / 2, (a.y + c.y) / 2, size / 2, n - 1);
 }
 
 void ofApp::levelsColor(int l){
