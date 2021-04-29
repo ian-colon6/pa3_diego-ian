0f5103dc5c3f3b8302b55e834593c8a897e82190
diegomartinez13 <diego.martinez13@upr.edu>
2021-04-28 20:49:29 -0400

    RandomAngle added

--- src/drawMode2.cpp
+++ src/drawMode2.cpp
@@ -9,7 +9,14 @@ drawMode2::drawMode2(){
 }
 
 void drawMode2::setActive(bool activated){
-
+    //Changes the angles of the recursion levels each time its activated 
+    if(activated){
+        d0 = ofRandom(30,70);
+        d1 = ofRandom(30,70);
+        d2 = ofRandom(30,70);
+        d3 = ofRandom(30,70);
+        d4 = ofRandom(30,70);
+    }
     this->isActive_2 = activated;
 }
 
@@ -26,12 +33,19 @@ void drawMode2::levelsColor(int l){
     else if(l%5 == 4)ofSetColor(255,255,0);
 }
 
+int drawMode2::randomAngle(int l){ //Adds the ange to each recursion level
+    if(l%5 == 0)return d0;
+    else if(l%5 == 1)return d1;
+    else if(l%5 == 2)return d2;
+    else if(l%5 == 3)return d3;
+    else if(l%5 == 4)return d4;
+}
 void drawMode2::draw(int levels){
-    drawHelper2(length, levels, x, y, deg);    
+    drawHelper2(length, levels, x, y);    
 }
 
-void drawMode2::drawHelper2(int length, int n, int x, int y, int d){
-
+void drawMode2::drawHelper2(int length, int n, int x, int y){
+    d = randomAngle(n);
     levelsColor(n);
     if(n != 0){
         int middleY = y-length;
@@ -44,7 +58,7 @@ void drawMode2::drawHelper2(int length, int n, int x, int y, int d){
         ofDrawLine(x, y-length, rightBranchX, rightBranchY);
         ofDrawLine(x,y-length, leftBranchX, leftBranchY);
 
-        drawHelper2(length/2, n-1,rightBranchX,rightBranchY, deg);
-        drawHelper2(length/2,n-1,leftBranchX,leftBranchY, deg);
+        drawHelper2(length/2, n-1,rightBranchX,rightBranchY);
+        drawHelper2(length/2,n-1,leftBranchX,leftBranchY);
     }
 }
